361346f2fb248e2e7c2d3d193d6b2c36528bebb0
Zach Schneider <me@zachschneider.com>
2022-12-15 12:55:27 -0600

    Digitals Cool App

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000
+  },
   preview: {
-    port: 3000,
+    port: 3000
   },
   plugins: [react()]
 })
